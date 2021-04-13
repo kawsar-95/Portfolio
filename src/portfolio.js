ef8944c14ef6786651dcd8fdc2067db42b7c035b
kawsar-95 <virtualkawsar@gmail.com>
2021-04-13 14:44:29 +0600

    done

---
+++ src/portfolio.js
@@ -0,0 +1,182 @@
+import emoji from 'react-easy-emoji';
+import googlelogo from './assets/img/icons/common/google.svg';
+
+
+export const greetings = {
+    "name": "Nuruddin Kawsar",
+    "title": "Hi all, I'm Kawsar",
+    "description": "A passionate Full Stack Web Developer and Mobile App Developer having an experience of building Web applications with JavaScript / Reactjs / Nodejs / Python / Django / Flask and some other cool libraries and frameworks and Cross Platform Mobile Apps With Flutter.",
+    "resumeLink": ""
+}
+
+export const openSource = {
+  githubUserName: 'kawsar-95',
+};
+
+export const contact = {
+
+}
+
+export const socialLinks = {
+    "facebook": "https://www.facebook.com/",
+    "instagram": "https://www.instagram.com/",
+    "twitter": "https://twitter.com/",
+    "github": "https://github.com/",
+    "linkedin": "https://www.linkedin.com/in/"
+}
+
+export const skillsSection = {
+    title: 'What I do',
+    subTitle: 'A FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK',
+    skills: [
+      emoji(
+        '⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications'
+      ),
+      emoji('⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks'),
+      emoji(
+        '⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean'
+      ),
+    ],
+
+    softwareSkills: [
+      {
+        skillName: 'html-5',
+        fontAwesomeClassname: 'vscode-icons:file-type-html',
+      },
+      {
+        skillName: 'css3',
+        fontAwesomeClassname: 'vscode-icons:file-type-css',
+      },
+      {
+        skillName: 'sass',
+        fontAwesomeClassname: 'logos:sass',
+      },
+      {
+        skillName: 'JavaScript',
+        fontAwesomeClassname: 'logos:javascript',
+      },
+      {
+        skillName: 'TypeScript',
+        fontAwesomeClassname: 'logos:typescript-icon',
+      },
+      {
+        skillName: 'reactjs',
+        fontAwesomeClassname: 'vscode-icons:file-type-reactjs',
+      },
+      {
+        skillName: 'nodejs',
+        fontAwesomeClassname: 'logos:nodejs-icon',
+      },
+      {
+        skillName: 'flutter',
+        fontAwesomeClassname: 'logos:flutter',
+      },
+      {
+        skillName: 'npm',
+        fontAwesomeClassname: 'vscode-icons:file-type-npm',
+      },
+      {
+        skillName: 'mongoDB',
+        fontAwesomeClassname: 'vscode-icons:file-type-mongo',
+      },
+      {
+        skillName: 'aws',
+        fontAwesomeClassname: 'logos:aws',
+      },
+      {
+        skillName: 'firebase',
+        fontAwesomeClassname: 'logos:firebase',
+      },
+      {
+        skillName: 'python',
+        fontAwesomeClassname: 'logos:python',
+      },
+      {
+        skillName: 'git',
+        fontAwesomeClassname: 'logos:git-icon',
+      },
+
+    ],
+}
+
+
+export const SkillBars = [
+    {
+      Stack: 'Frontend', //Insert stack or technology you have experience in
+      progressPercentage: '80', //Insert relative proficiency in percentage
+    },
+    {
+      Stack: 'Backend',
+      progressPercentage: '60',
+    },
+    {
+      Stack: 'Programming',
+      progressPercentage: '60',
+    },
+  ]
+
+export const educationInfo = [
+    {
+      schoolName: 'Port City International University,Chittagong.',
+      subHeader: 'Bachelor of Science in Computer Science',
+      duration: 'January 2016 - August 2020',
+      desc: 'Participated in the research of  and published 3 papers.',
+      descBullets: [
+        'Lorem ipsum dolor sit amet, consectetur adipdfgiscing elit',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+      ],
+    },
+    // {
+    //   schoolName: 'Islamia College,Chittagong',
+    //   subHeader: 'HSC-Science',
+    //   duration: 'June 2012 - August 2014',
+    //   desc: 'Studied Science',
+    //   descBullets: [
+    //     '',
+    //     '',
+    //   ],
+    // },
+    // {
+    //   schoolName: 'Stanford University',
+    //   subHeader: 'Bachelor of Science in Computer Science',
+    //   duration: 'September 2013 - April 2017',
+    //   desc:
+    //     'Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...',
+    //   descBullets: ['Lorem ipsum dolorfdg sit amet, consectetur adipiscing elit'],
+    // },
+]
+
+export const experience = [
+  {
+    role: 'Frontend Developer',
+    company: 'Octapia',
+    companylogo: googlelogo,
+    date: 'October 2020 – Present',
+    desc:
+      '',
+    descBullets: [
+      '',
+      '',
+    ],
+  },
+
+]
+
+export const projects = [
+  {
+    name: "lorem ipsum",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+  },
+  {
+    name: "lorem ipsum",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+  },
+  {
+    name: "lorem ipsum",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    link: {
+      name: "hanzla",
+      url: "kasjfklsdjf"
+    }
+  }
+]
